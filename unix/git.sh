41266f51864558152c45603d2fd078665dd05e3a
smjrifle <smjrifle@gmail.com>
2015-07-15 12:16:02 +0545

    Added git pull in all folder and git status add commit pull push script

--- unix/git.sh
+++ unix/git.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
-   git status
-     read -p "Do you want to push: " yn
-       case $yn in
-         [Yy]* ) git add -A;git commit -m $1;git pull origin master;git push origin master; break;;
-           [Nn]* ) exit;;
-             * ) echo "Please answer yes or no:: ";;
-               esac
+git status
+read -p "Do you want to push: " yn
+case $yn in
+	[Yy]* ) git add -A;git commit -m $1;git pull origin master;git push origin master; break;;
+	[Nn]* ) exit;;
+	* ) echo "Please answer yes or no:: ";;
+esac
